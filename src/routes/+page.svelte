d93878566a580cd103d4317b46d09c8fb6aa2d8c
d57alpha <d57alpha@gmail.com>
2026-05-04 04:30:10 -0700

    Add more edu<->plugin messaging

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,65 @@
+
+<!-- Svelte 5 style -->
+ <svelte:window onmessage={(event) => {
+     //if (event.origin !== 'http://localhost:5174') return;
+     if (event.origin !== 'https://eduplatform-sigma-nine.vercel.app/') return;
+     console.log('plugin, get eduplatform message', event.data);
+ }} />
+
+ <!--
+
+  Development -> Vercel
+    http://localhost:5173/ -> https://jdilearning.vercel.app/
+    http://localhost:5174/ -> https://eduplatform-sigma-nine.vercel.app/
+
+  Vercel -> Development
+    https://jdilearning.vercel.app/ -> http://localhost:5173/ 
+    https://eduplatform-sigma-nine.vercel.app/ -> http://localhost:5174/
+
+ -->
+
 <script>
     import { onMount } from "svelte";
 
   	onMount(() => {
  		  console.log('jdi learning (child) component has mounted');
-
       //  Let the Parent (Education system) know the plugin is active
-
-        // This is for plugin in iframe
-        //window.parent.postMessage({ type: 'IFRAME_READY' }, 'http://localhost:5174/');
-        window.parent.postMessage({ type: 'IFRAME_READY' }, 'https://eduplatform-sigma-nine.vercel.app/');
-
-        // This is for plugin in separate window
-        //window.opener.postMessage({ type: 'IFRAME_READY' }, 'http://localhost:5174/');
-
-        window.addEventListener('message', (event) => {
-          // Security Check: Always verify the origin!
-          // if (event.origin !== "https://trusted-domain.com") return;
-          //console.log('Received data:', event.data.payload);
-          console.log('jdi learning (child) Received data:', event.data, event.origin);
-      });
+        //localhost
+      //window.parent.postMessage({ type: 'IFRAME_READY' }, 'http://localhost:5174/');
+      // Vercel
+      window.parent.postMessage({ type: 'IFRAME_READY' }, 'https://eduplatform-sigma-nine.vercel.app/');
 	  });
 
-    // This is for plugin in iframe
     function sendmsg1(){
+      // localhost
       //window.parent.postMessage("Message 1 from plugin to parent", 'http://localhost:5174/');
+      // Vercel
       window.parent.postMessage("Message 1 from plugin to parent", 'https://eduplatform-sigma-nine.vercel.app/');
     }
     function sendmsg2(){
+      // localhost
       //window.parent.postMessage("Message 2 from plugin to parent", 'http://localhost:5174/');
+      // Vercel
       window.parent.postMessage("Message 2 from plugin to parent", 'https://eduplatform-sigma-nine.vercel.app/');
     }
 
-    // This is for plugin in separate window
-    /*
-    function sendmsg1(){
-      window.opener.postMessage("Message 1 from plugin to parent", 'http://localhost:5174/');
-    }
-    function sendmsg2(){
-      window.opener.postMessage("Message 2 from plugin to parent", 'http://localhost:5174/');
-    }
-    */
-   let b = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1"
 </script>
 
 <h1>JDI Learning</h1>
-<button class={b}  onclick={sendmsg1}>Send Message 1 to Parent Education</button>
-<button class={b}  onclick={sendmsg2}>Send Message 2 to Parent Education</button>
+<button class="btn btn-soft btn-primary m-1"  onclick={sendmsg1}>Send Message 1 to Parent Education</button>
+<button class="btn btn-soft btn-primary m-1"  onclick={sendmsg2}>Send Message 2 to Parent Education</button>
+
+<!--
+
+// This is for plugin in separate window
+//window.opener.postMessage({ type: 'IFRAME_READY' }, 'http://localhost:5174/');
+// This is for plugin in separate window
+/*
+function sendmsg1(){
+  window.opener.postMessage("Message 1 from plugin to parent", 'http://localhost:5174/');
+}
+function sendmsg2(){
+  window.opener.postMessage("Message 2 from plugin to parent", 'http://localhost:5174/');
+}
+*/
+-->
