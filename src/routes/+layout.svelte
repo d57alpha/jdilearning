3bf701c42817552072d679db1c4adf19760bdb4d
d57alpha <d57alpha@gmail.com>
2026-05-04 12:56:16 -0700

    Set origin variables in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,19 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
+
+  //  this section sets the origins for communication between
+  //  parent (education platform) and iframe (jdilearning)
+  //  for localhost (development) and production (Vercel)
+
+    import { setContext } from 'svelte';
+    //  localhost development
+    //setContext("eduplatform", "http://localhost:5174");
+    //setContext("jdilearning", "http://localhost:5173");
+    //  Vercel deployment
+    setContext("eduplatform", "https://eduplatform-sigma-nine.vercel.app");
+    setContext("jdilearning", "https://jdilearning.vercel.app");
+
 	let { children } = $props();
 </script>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,50 +1,34 @@
 
-<!-- Svelte 5 style -->
- <svelte:window onmessage={(event) => {
-     //if (event.origin !== 'http://localhost:5174') return;
-     console.log('plugin, try to get eduplatform message', event.origin, event.data);
-     if (event.origin !== 'https://eduplatform-sigma-nine.vercel.app') return;
-     console.log('plugin, got eduplatform message', event.data);
- }} />
-
- <!--
-
-  Development -> Vercel
-    http://localhost:5173/ -> https://jdilearning.vercel.app/
-    http://localhost:5174/ -> https://eduplatform-sigma-nine.vercel.app/
-
-  Vercel -> Development
-    https://jdilearning.vercel.app/ -> http://localhost:5173/ 
-    https://eduplatform-sigma-nine.vercel.app/ -> http://localhost:5174/
-
- -->
-
 <script>
+    import { getContext } from 'svelte';
+    let eduplatform = getContext("eduplatform");
+    let jdilearning = getContext("jdilearning");
+
+    console.log('in jdilearning, whats eduplatform', eduplatform)
+    console.log('in jdilearning, whats jdilearning', jdilearning)
+
     import { onMount } from "svelte";
 
   	onMount(() => {
- 		  console.log('jdi learning (child) component has mounted');
-      //  Let the Parent (Education system) know the plugin is active
-        //localhost
-      //window.parent.postMessage({ type: 'IFRAME_READY' }, 'http://localhost:5174/');
-      // Vercel
-      window.parent.postMessage({ type: 'IFRAME_READY' }, 'https://eduplatform-sigma-nine.vercel.app');
+      window.parent.postMessage({ type: 'IFRAME_READY' }, eduplatform);
 	  });
 
     function sendmsg1(){
-      // localhost
-      //window.parent.postMessage("Message 1 from plugin to parent", 'http://localhost:5174/');
-      // Vercel
-      window.parent.postMessage("Message 1 from plugin to parent", 'https://eduplatform-sigma-nine.vercel.app');
+      window.parent.postMessage("Message 1 from plugin to parent", eduplatform);
     }
     function sendmsg2(){
-      // localhost
-      //window.parent.postMessage("Message 2 from plugin to parent", 'http://localhost:5174/');
-      // Vercel
-      window.parent.postMessage("Message 2 from plugin to parent", 'https://eduplatform-sigma-nine.vercel.app');
+      window.parent.postMessage("Message 2 from plugin to parent", eduplatform);
     }
 
 </script>
+
+<!-- Svelte 5 style -->
+<svelte:window onmessage={(event) => {
+  console.log('plugin, try to get eduplatform message', event.origin, event.data);
+  if (event.origin !== eduplatform) return;
+  console.log('plugin, got eduplatform message', event.data);
+ }}
+ />
 
 <h1>JDI Learning</h1>
 <button class="btn btn-soft btn-primary m-1"  onclick={sendmsg1}>Send Message 1 to Parent Education</button>
@@ -53,14 +37,14 @@
 <!--
 
 // This is for plugin in separate window
-//window.opener.postMessage({ type: 'IFRAME_READY' }, 'http://localhost:5174/');
+//window.opener.postMessage({ type: 'IFRAME_READY' }, eduplatform);
 // This is for plugin in separate window
 /*
 function sendmsg1(){
-  window.opener.postMessage("Message 1 from plugin to parent", 'http://localhost:5174/');
+  window.opener.postMessage("Message 1 from plugin to parent", eduplatform);
 }
 function sendmsg2(){
-  window.opener.postMessage("Message 2 from plugin to parent", 'http://localhost:5174/');
+  window.opener.postMessage("Message 2 from plugin to parent", eduplatform);
 }
 */
 -->
